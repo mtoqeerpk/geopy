42d69b2a7bb19d51830804fd98b0b840546869c0
GeoPy Team <47583789+geopy4ep@users.noreply.github.com>
2019-02-16 20:41:55 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#############################################################################################
+#                                                                                           #
+# Author:   Haibin Di                                                                       #
+# Date:     March 2018                                                                      #
+#                                                                                           #
+#############################################################################################
+
+# Shape all as a single class
+
+__all__ = ['gui']
+
+import os, sys
+#
+sys.path.append(os.path.dirname(__file__))
+from src.gui.gui_main import gui_main as gui
+
+if __name__ == "__main__":
+    gui()
